bc75a0ead545360e69c5b002f31fe8e07b9f77a5
austingero <43484848+austingero@users.noreply.github.com>
2018-09-23 12:54:08 -0400

    Main Menu, level Design

--- Game Asset Jam Project/Assets/MainMenu.cs
+++ Game Asset Jam Project/Assets/MainMenu.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class StartButton : MonoBehaviour {
+public class MainMenu: MonoBehaviour {
 
 	public void StartGame()
     {
